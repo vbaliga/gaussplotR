786465a820ea377aa275f8560a9a54f25d484d2c
Vikram Baliga <vbaliga87@gmail.com>
2020-09-19 14:33:30 -0700

    add example data

---
+++ R/gaussplot_sample_data.R
@@ -0,0 +1,20 @@
+#' Sample data set
+#'
+#' A \code{data.frame} of raw data and fitted 2D gaussian parameters; intended
+#' for use with \code{predict_gaussian_2D()}
+#'
+#' @format A data frame with 36 rows and 11 variables:
+#' \describe{
+#'   \item{X_values}{vector of numeric values for the x-axis}
+#'   \item{Y_values}{vector of numeric values for the y-axis}
+#'   \item{response}{vector of numeric valeus for the response variable}
+#'   \item{norm_g_resp}{normalized values from the 2D gaussian fit}
+#'   \item{g_resp}{values from the 2D gaussian fit}
+#'   \item{A}{amplitude of 2D gaussian (repeated)}
+#'   \item{X_peak}{location of peak x-axis value (repeated)}
+#'   \item{X_var}{variance in x (repeated)}
+#'   \item{Q}{orientation parameter of the gaussian (repeated)}
+#'   \item{Y_peak}{location of peak y-axis value (repeated)}
+#'   \item{Y_var}{variance in y (repeated)}
+#' }
+"gaussplot_sample_data"
--- R/predict_gaussian_2D.R
+++ R/predict_gaussian_2D.R
@@ -23,6 +23,29 @@
 #' @author Vikram B. Baliga
 #'
 #' @export
+#'
+#' @examples
+#' ## Load the sample data set
+#' data(gaussplot_sample_data)
+#'
+#' ## Generate a grid of x- and y- values on which to predict
+#' grid <-
+#'   expand.grid(X_values = seq(from = -5, to = 0, by = 0.1),
+#'               Y_values = seq(from = -5, to = 4, by = 0.1))
+#'
+#' ## Predict the values using predict_gaussian_2D
+#' predicted_values <-
+#'   predict_gaussian_2D(
+#'     X_values = grid$X_values,
+#'     Y_values = grid$Y_values,
+#'     A = gaussplot_sample_data$A[1],
+#'     X_peak = gaussplot_sample_data$X_peak[1],
+#'     Y_peak = gaussplot_sample_data$Y_peak[1],
+#'     Q = gaussplot_sample_data$Q[1],
+#'     X_var = gaussplot_sample_data$X_var[1],
+#'     Y_var = gaussplot_sample_data$Y_var[1]
+#'   )
+
 
 predict_gaussian_2D <- function(X_values,
                                 Y_values,
